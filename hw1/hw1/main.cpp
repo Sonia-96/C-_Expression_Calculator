7d41eadb8c9fcd911cd5182856292b7b24dcde65
Sonia-96 <672365639@qq.com>
2023-01-19 10:51:57 -0700

    add hw1

---
+++ hw1/hw1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by Yue Sun on 1/12/23.
+//
+
+#include <iostream>
+#include "cmdline.h"
+
+int main(int argc, const char * argv[]) {
+    use_arguments(argc, argv);
+    return 0;
+}
