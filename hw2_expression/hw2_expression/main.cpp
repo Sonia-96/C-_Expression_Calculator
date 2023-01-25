06a7b0e73af16dfc335982480b124099359d0f3b
Sonia-96 <672365639@qq.com>
2023-01-24 22:21:57 -0700

    finished 3 methods

--- hw2_expression/hw2_expression/main.cpp
+++ hw2_expression/hw2_expression/main.cpp
@@ -5,13 +5,19 @@
 //  Created by Yue Sun on 1/12/23.
 //
 
+#include <iostream>
 #include "cmdline.h"
+#include "expr.hpp"
 
 int add(int a, int b) {
     return a + b;
 }
 
 int main(int argc, const char * argv[]) {
+//    Num num(1);
+//    Add add(1, 2);
+//    std::cout << typeid(num).name() << std::endl;
+//    std::cout << typeid(add).name() << std::endl;
     use_arguments(argc, argv);
     return 0;
 }
--- hw2_expression/hw2_expression/tests.cpp
+++ hw2_expression/hw2_expression/tests.cpp
@@ -10,10 +10,10 @@
 
 TEST_CASE("Equals") {
     SECTION("Num") {
-        Num num1(1);
-        Num num2(2);
-        Num num3(1);
-        Num num4(-1);
+        NumExpr num1(1);
+        NumExpr num2(2);
+        NumExpr num3(1);
+        NumExpr num4(-1);
         CHECK_FALSE(num1.equals(&num2));
         CHECK_FALSE(num2.equals(&num1));
         CHECK(num1.equals(&num3));
@@ -24,9 +24,9 @@ TEST_CASE("Equals") {
     }
     
     SECTION("Add_simple") {
-        Add add1(new Num(1), new Num(2));
-        Add add2(new Num(2), new Num(1));
-        Add add3(new Num(1), new Num(2));
+        AddExpr add1(new NumExpr(1), new NumExpr(2));
+        AddExpr add2(new NumExpr(2), new NumExpr(1));
+        AddExpr add3(new NumExpr(1), new NumExpr(2));
         CHECK_FALSE(add1.equals(&add2));
         CHECK_FALSE(add2.equals(&add1));
         CHECK(add1.equals(&add3));
@@ -36,9 +36,9 @@ TEST_CASE("Equals") {
     }
     
     SECTION("Add_nested") {
-        Add add1(new Add(new Num(1), new Num(2)), new Mult(new Num(3), new Num(4)));
-        Add add2(new Mult(new Num(3), new Num(4)), new Add(new Num(1), new Num(2)));
-        Add add3(new Add(new Num(1), new Num(2)), new Mult(new Num(3), new Num(4)));
+        AddExpr add1(new AddExpr(new NumExpr(1), new NumExpr(2)), new MultExpr(new NumExpr(3), new NumExpr(4)));
+        AddExpr add2(new MultExpr(new NumExpr(3), new NumExpr(4)), new AddExpr(new NumExpr(1), new NumExpr(2)));
+        AddExpr add3(new AddExpr(new NumExpr(1), new NumExpr(2)), new MultExpr(new NumExpr(3), new NumExpr(4)));
         CHECK_FALSE(add1.equals(&add2));
         CHECK_FALSE(add2.equals(&add1));
         CHECK(add1.equals(&add3));
@@ -48,9 +48,9 @@ TEST_CASE("Equals") {
     }
 
     SECTION("Mult_simple") {
-        Mult mult1(new Num(1), new Num(2));
-        Mult mult2(new Num(2), new Num(1));
-        Mult mult3(new Num(1), new Num(2));
+        MultExpr mult1(new NumExpr(1), new NumExpr(2));
+        MultExpr mult2(new NumExpr(2), new NumExpr(1));
+        MultExpr mult3(new NumExpr(1), new NumExpr(2));
         CHECK_FALSE(mult1.equals(&mult2));
         CHECK_FALSE(mult2.equals(&mult1));
         CHECK(mult1.equals(&mult3));
@@ -60,9 +60,9 @@ TEST_CASE("Equals") {
     }
     
     SECTION("Mult_nested") {
-        Mult mult1(new Add(new Num(1), new Num(2)), new Mult(new Num(3), new Num(4)));
-        Mult mult2(new Mult(new Num(3), new Num(4)), new Add(new Num(1), new Num(2)));
-        Mult mult3(new Add(new Num(1), new Num(2)), new Mult(new Num(3), new Num(4)));
+        MultExpr mult1(new AddExpr(new NumExpr(1), new NumExpr(2)), new MultExpr(new NumExpr(3), new NumExpr(4)));
+        MultExpr mult2(new MultExpr(new NumExpr(3), new NumExpr(4)), new AddExpr(new NumExpr(1), new NumExpr(2)));
+        MultExpr mult3(new AddExpr(new NumExpr(1), new NumExpr(2)), new MultExpr(new NumExpr(3), new NumExpr(4)));
         CHECK_FALSE(mult1.equals(&mult2));
         CHECK_FALSE(mult2.equals(&mult1));
         CHECK(mult1.equals(&mult3));
@@ -72,11 +72,76 @@ TEST_CASE("Equals") {
     }
 
     SECTION("Variable") {
-        Variable var1("a");
-        Variable var2("1");
-        Variable var3("a");
+        VarExpr var1("a");
+        VarExpr var2("1");
+        VarExpr var3("a");
         CHECK_FALSE(var1.equals(&var2));
         CHECK(var1.equals(&var3));
         CHECK(!var2.equals(&var3));
     }
 };
+
+TEST_CASE("Interp") {
+    SECTION("Num") {
+        CHECK((new NumExpr(0)) -> interp() == 0);
+        CHECK((new NumExpr(-100))-> interp() == -100);
+    }
+    
+    SECTION("Add_simple") {
+        
+    }
+    
+    SECTION("Add_nested") {
+        
+    }
+    
+    SECTION("Mult_simple") {
+        MultExpr mult1(new NumExpr(3), new NumExpr(2));
+        CHECK(mult1.interp() == 6);
+    }
+    
+    SECTION("Mult_nested") {
+        MultExpr mult1(new AddExpr(2, 3), new MultExpr(4, 5));
+        CHECK(mult1.interp() == 100);
+    }
+    
+    SECTION("Variable") {
+        VarExpr var1("x");
+        CHECK_THROWS_WITH(var1.interp(), "A variable has no value!");
+    }
+}
+
+TEST_CASE("has_variable") {
+    SECTION("Num") {
+        
+    }
+    
+    SECTION("Add_simple") {
+        
+    }
+    
+    SECTION("Add_nested") {
+        
+    }
+    
+    SECTION("Mult_simple") {
+        MultExpr mult1(new NumExpr(3), new NumExpr(2));
+    }
+    
+    SECTION("Mult_nested") {
+        MultExpr mult1(new AddExpr(2, 3), new MultExpr(4, 5));
+    }
+    
+    SECTION("Variable") {
+        
+    }
+}
+
+TEST_CASE("subst") {
+    CHECK( (new AddExpr(new VarExpr("x"), new NumExpr(7)))
+           ->subst("x", new VarExpr("y"))
+           ->equals(new AddExpr(new VarExpr("y"), new NumExpr(7))) );
+    CHECK( (new VarExpr("x"))
+           ->subst("x", new AddExpr(new VarExpr("y"),new NumExpr(7)))
+           ->equals(new AddExpr(new VarExpr("y"),new NumExpr(7))) );
+}
